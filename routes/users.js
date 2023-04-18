093d7dbc2d003aaa0dad923b0577b6da80c55641
JureKotnik <jure2002k@gmail.com>
2023-04-18 14:21:47 +0200

    Middleware

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(logger);
 
 router.get('/', (req, res) => {
     res.send('User List');
@@ -29,5 +30,10 @@ router.param('id', (req, res, next, id) => {
     req.user = users[id];
     next();
 });
+
+function logger(req, res, next){
+    console.log(req.originalUrl);
+    next();
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
-    console.log('Here');
     res.render('index', {text: 'World'});
 });
 
