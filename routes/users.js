3b7e63ab9770fa73e97abdc79fc053ad3a5efd4d
JureKotnik <jure2002k@gmail.com>
2023-04-18 14:39:38 +0200

    Parsing Form/ JSON Data

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,18 @@ router.get('/', (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-    res.send('User New Form');
+    res.render('users/new');
 });
 
 router.post('/', (req, res) =>{
-    res.send('Create user');
+    const isValid = true;
+    if(isValid){
+        users.push({firstName: req.body.firstName});
+        res.redirect(`/users/${users.length - 1}`);
+    } else{
+        console.log('Error');
+        res.render('users/new', {firstName: req.body.firstName});
+    }
 });
 
 router.route('/:id').get((req, res) =>{
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 
