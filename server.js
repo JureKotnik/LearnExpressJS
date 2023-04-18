572bc9df539f53cee01460c00d148e53c334c02b
JureKotnik <jure2002k@gmail.com>
2023-04-18 14:25:39 +0200

    Rendering static Files

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) =>{
-    res.render('index', {text: 'World'});
-});
-
 
 const userRouter = require('./routes/users');
 
