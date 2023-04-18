73237873ea1e0e0ad0aea7533aac495a570c2abe
JureKotnik <jure2002k@gmail.com>
2023-04-18 14:13:26 +0200

    Advanced routing

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,24 @@ router.get('/new', (req, res) => {
     res.send('User New Form');
 });
 
+router.post('/', (req, res) =>{
+    res.send('Create user');
+});
+
+router.route('/:id').get((req, res) =>{
+    console.log(req.user);
+    res.send(`Get User With ID ${req.params.id}`);
+}).put((req, res) =>{
+    res.send(`Update User With ID ${req.params.id}`);
+}).delete((req, res) =>{
+    res.send(`Delete User With ID ${req.params.id}`);
+});
+
+const users = [{name: 'Kyle'}, {name: 'Sally'}];
+router.param('id', (req, res, next, id) => {
+    console.log(id);
+    req.user = users[id];
+    next();
+});
 
 module.exports = router;
