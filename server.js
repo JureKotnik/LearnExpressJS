5ad41c8eef1b67b4a73df408b3e57bbc5406409a
JureKotnik <jure2002k@gmail.com>
2023-04-18 13:35:51 +0200

    Sending Data

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get('/', (req, res) =>{
     console.log('Here');
+    //res.status(500).json({message: 'Error'});
+    res.download('server.js');
     res.send("Hi");
 });
 
